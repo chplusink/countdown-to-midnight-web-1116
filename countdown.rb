04d63800a20099e4661902ce114d304fd9accab8
Vic Lam <lam.vic.k@gmail.com>
2016-11-23 22:47:37 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
 
-def countdown
+def countdown(segundos)
+  while segundos > 0 do
+    puts "#{segundos} SECOND(S)!"
+    segundos -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(segundos)
+  for s in 1..segundos do
+    puts "#{segundos - s + 1} SECOND(S)!"
+    sleep(1)
+  end
+  return "HAPPY NEW YEAR"
 end
